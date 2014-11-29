c160a8b0fc11b09e5a40905d9ba0fe02b98f557a
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-11-29 23:42:25 +0800

    use manage.js now

---
+++ manage.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+if (require.main === module) {
+  var yargs = require("yargs");
+
+  function runserver(args) {
+    var argv = yargs(args)
+      .options("p", {alias: "port", default: 3000, requiresArg: true})
+      .argv;
+    var app = require("./app");
+    console.log("Connecting to MongoDB...");
+    app.db.on("error", function(err) {
+      console.log("Error connection to MongoDB:", err);
+    });
+
+    app.db.once("open", function() {
+      console.log("Connected to MongoDB !");
+      console.log();
+      console.log("Starting HTTP server...");
+      app.app.listen(argv.p, function() {
+        console.log("HTTP server started on port %d !", argv.p);
+        console.log();
+      });
+    });
+  }
+
+  switch (process.argv[2]) {
+    case "runserver":
+      return runserver(process.argv.slice(3));
+    case undefined:
+      console.error("Provide a command please");
+      process.exit(1);
+    default:
+      console.error("Command %s unknown", process.argv[2]);
+  }
+}
--- script/prod_first_launch.sh
+++ script/prod_first_launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-NODE_ENV=production exec forever start ./start.js
+NODE_ENV=production exec forever start ./manage.js runserver
