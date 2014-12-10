6deed6afdabd9b2ed1ddd6dd18aa0a0908ce986d
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-10 18:01:44 +0800

    add mongoose in tests

--- api/user/_tests.js
+++ api/user/_tests.js
@@ -1,8 +1,13 @@
 var chai = require("chai");
 chai.should();
+var mongoose = require("mongoose");
+var conn = mongoose.connection;
 
 describe("User", function() {
   describe("test", function() {
+    before("clean the db", function(done) {
+      conn.db.dropDatabase(done);
+    });
     it("should be equal 42", function(done) {
       setImmediate(function() {
         var nb = 42;
--- commands/test.js
+++ commands/test.js
@@ -3,6 +3,7 @@ function test(args) {
   var path = require("path");
   var api = require("../api");
   var Mocha = require("mocha");
+  var mongoose = require("mongoose");
   var mocha = new Mocha;
 
   for (name in api) {
@@ -12,11 +13,26 @@ function test(args) {
     }
   }
 
-  mocha.run(function(failures){
-    process.on('exit', function () {
-      process.exit(failures);
+  var conn = mongoose.connection;
+
+  conn.on("error", function(err) {
+    console.log("Error connection to MongoDB:", err);
+  });
+
+  conn.once("open", function() {
+    console.log("Connected to MongoDB !");
+    mocha.run(function(failures) {
+      conn.db.dropDatabase(function() {
+        mongoose.disconnect();  
+      });
+      process.on('exit', function () {
+        process.exit(failures);
+      });
     });
   });
+
+  console.log("Connecting to MongoDB...");
+  mongoose.connect("mongodb://127.0.0.1/nourriture_test");
 }
 
 module.exports = {
