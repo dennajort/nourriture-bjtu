6b23fe101555fe4f6cfd19eec88c12486fdc3135
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-14 11:26:56 +0800

    fix again

--- script/prod_install_init_script.sh
+++ script/prod_install_init_script.sh
@@ -3,5 +3,4 @@
 cp ./script/pm2_nourriture.sh /etc/init.d/pm2_nourriture
 chmod +x /etc/init.d/pm2_nourriture
 update-rc.d pm2_nourriture defaults
-service pm2_nourriture enable
 service pm2_nourriture start
