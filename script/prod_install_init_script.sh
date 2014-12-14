a75d119961c46979848cb882d15199f6204f513b
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-14 11:13:32 +0800

    ok works !

--- script/prod_install_init_script.sh
+++ script/prod_install_init_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp ./script/pm2_nourriture.sh /etc/init.d/
-chmod +x /etc/init.d/pm2_nourriture.sh
+cp ./script/pm2_nourriture.sh /etc/init.d/pm2_nourriture
+chmod +x /etc/init.d/pm2_nourriture
 update-rc.d pm2_nourriture defaults
 update-rc.d pm2_nourriture enable
+update-rc.d pm2_nourriture start
