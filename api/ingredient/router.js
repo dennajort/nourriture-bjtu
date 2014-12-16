a00b399ca24d217ba0c4f1cde03eed8059daaadb
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-17 01:05:20 +0800

    remove authentication on remove ingredient

--- api/ingredient/router.js
+++ api/ingredient/router.js
@@ -38,6 +38,6 @@ router.route("/")
 router.route("/:oid")
   .get(common.rest.findOne(Ingredient))
   .put(common.policies.isSuperAdmin, common.rest.updateOne(Ingredient))
-  .delete(common.policies.isSuperAdmin, common.rest.removeOne(Ingredient));
+  .delete(common.rest.removeOne(Ingredient));
 
 module.exports = router;
