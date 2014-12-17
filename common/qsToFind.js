83d19b04a489d007d5a4f9082eae106548ff5022
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-17 14:08:36 +0800

    fix where in qsToFind

--- common/qsToFind.js
+++ common/qsToFind.js
@@ -2,7 +2,11 @@ function qsToFind(model, qs) {
   var ret = model;
   var per_page = 10;
   if (qs.per_page !== undefined) per_page = qs.per_page;
-  if (qs.where !== undefined) ret = ret.find(qs.where);
+  if (qs.where !== undefined) {
+    try {
+      ret = ret.find(JSON.parse(qs.where))
+    } catch(err) {}
+  }
   if (qs.sort !== undefined) {
     var v = qs.sort;
     ret = ret.sort((v.join !== undefined) ? v.join(" ") : v);
