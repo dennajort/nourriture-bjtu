43f217dd57e1ed29c27109d0deb27fb23e5b8a1e
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-17 18:05:05 +0800

    add /ingredient/:oid/update

--- api/ingredient/router.js
+++ api/ingredient/router.js
@@ -4,14 +4,16 @@ var multer = require("multer");
 
 var router = express.Router();
 
+var onFileUploadStart = function (file) {
+  if (file.fieldname != "photo") return false;
+  var mime_regex = /^image\//i;
+  if (!mime_regex.test(file.mimetype)) return false;
+};
+
 router.route("/create")
   .post(common.policies.isAuthenticated, multer({
     dest: Ingredient.PHOTO_DIR,
-    onFileUploadStart: function (file) {
-      if (file.fieldname != "photo") return false;
-      var mime_regex = /^image\//i;
-      if (!mime_regex.test(file.mimetype)) return false;
-    }
+    onFileUploadStart: onFileUploadStart
   }), function(req, res, next) {
     var data = _.omit(req.body, "photo_name");
     var ing = new Ingredient(data);
@@ -28,6 +30,34 @@ router.route("/create")
             res.json(ing);
           });
         }, next);
+    });
+  });
+
+router.route("/:oid/update")
+  .post(common.policies.isAuthenticated, multer({
+    dest: Ingredient.PHOTO_DIR,
+    onFileUploadStart: onFileUploadStart
+  }), function(req, res, next) {
+    if (!validObjectid(req.params.oid)) return next("route");
+    Ingredient.findById(req.params.oid, function(err, ing) {
+      if (err) return next(err);
+      if (ing === null) return next("route");
+      var data = _.omit(req.body, "photo_name");
+      _.extend(ing, data);
+      ing.save(function(err, ing) {
+        if (err && err.name == "ValidationError") return res.status(400).json(err);
+        if (err) return next(err);
+        var photo = req.files.photo
+        if (photo === undefined) return res.json(ing);
+        ing.changePhoto(photo.name)
+          .then(function() {
+            ing.save(function(err, ing) {
+              if (err && err.name == "ValidationError") return res.status(400).json(err);
+              if (err) return next(err);
+              res.json(ing);
+            });
+          }, next);
+      });
     });
   });
 
