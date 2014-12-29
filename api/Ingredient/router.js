67e9632f1e6c12b0326867b4135c707d2fc4adf6
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-29 23:43:10 +0800

    test url

--- api/Ingredient/router.js
+++ api/Ingredient/router.js
@@ -86,6 +86,12 @@ function ingredientAutocomplete(req, res, next) {
 	Ingredient.find({name: {"contains": req.query.name}})
 	.limit(10)
 	.populate("photo").then(function(ings) {
+		ings = _.mapValues(ings, function(ing) {
+			if (ing.photo && ing.photo.uri) {
+				ing.photo_url = "http://nourriture.dennajort.fr" + ing.photo.uri();
+			}
+			return ing;
+		})
 		res.json(ings);
 	}, next);
 }
