5f754af51d673ca32525c8123cd6823063bb4269
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2014-12-31 17:58:31 +0800

    fix timeline socket.io callback

--- api/Timeline/router.js
+++ api/Timeline/router.js
@@ -2,8 +2,10 @@ module.exports = function(pol) {
 	APP.on("DBEvent", function(Model, evt, data, user) {
 		idt = Model.identity;
 		if (idt == "ingredient") {
-			Timeline.create({name: evt, domain: idt, user: user, ingredient: data}).then(function(entry) {
-				APP.dbEvent(Timeline, "create", entry);
+			return Timeline.create({name: evt, domain: idt, user: user, ingredient: data}).then(function(entry) {
+				return Timeline.findOneById(entry.id).populate("user").then(function(entry) {
+					APP.dbEvent(Timeline, "create", entry);
+				})
 			});
 		}
 	});
