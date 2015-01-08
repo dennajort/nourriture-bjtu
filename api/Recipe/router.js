9d42287bc1c5115285b0fc6b2d95d776d35d7368
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-01-08 18:42:36 +0800

    fix add recipe

--- api/Recipe/router.js
+++ api/Recipe/router.js
@@ -38,13 +38,11 @@ function recipeCreate(req, res, next) {
 		res.json(rec);
 	}
 
-	console.log("BEFORE CREATE");
 	Recipe.create(data).then(function(rec) {
 		_.forEach(ingredients, function(ing) {
 			ing.recipe = rec.id;
 			rec.ingredients.add(ing);
 		});
-		console.log("BEFORE FIRST SAVE");
 		return rec.save().then(function(rec) {
 			var photo = req.files.photo;
 			if (photo === undefined || !isImage(photo)) return finish(rec);
@@ -54,6 +52,7 @@ function recipeCreate(req, res, next) {
 					if (err) return next(err);
 					rec.photo = up;
 					console.log("BEFORE LAST SAVE");
+					console.log(rec);
 					rec.save().then(function(rec) {
 						finish(rec);
 					}, ValCb(res, next))
