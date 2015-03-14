067741f1dcd7e6d81464e1f7b1768abc065c6c2d
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-03-15 00:10:01 +0800

    fix build

--- prod/build_and_up.sh
+++ prod/build_and_up.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 ./compose.sh build && ./compose.sh up -d &&\
-docker rmi $(docker images -q -f "dangling=true")
+DANGLING_IMAGES=$(docker images -q -f "dangling=true") &&\
+if [ -n "$DANGLING_IMAGES" ]; then
+  docker rmi $DANGLING_IMAGES
+fi
