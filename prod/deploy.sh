eae8004d25ac30435bca354a07290b612951e7e4
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-03-17 18:50:27 +0800

    remove and update image fix

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -2,5 +2,11 @@
 scp ./prod/docker-compose.yml nourriture-prod@nourriture.dennajort.fr:docker-compose.yml && \
 ssh nourriture-prod@nourriture.dennajort.fr <<EOF
   docker-compose -p "nourriturebjtu" pull --allow-insecure-ssl && \
-  docker-compose -p "nourriturebjtu" up -d
+  docker pull docker.dennajort.fr/nourriturebjtu_api && \
+  docker pull docker.dennajort.fr/nourriturebjtu_nginx && \
+  docker-compose -p "nourriturebjtu" up -d && \
+  DANGLING_IMAGES=$(docker images -q -f "dangling=true") && \
+  if [ -n "$DANGLING_IMAGES" ]; then
+    docker rmi $DANGLING_IMAGES
+  fi
 EOF
