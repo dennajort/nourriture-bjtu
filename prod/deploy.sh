b8a7ddd4ead31656004cbf96999e56eb73174112
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-03-17 18:09:04 +0800

    add insecure ssl

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp ./prod/docker-compose.yml nourriture-prod@nourriture.dennajort.fr:docker-compose.yml && \
 ssh nourriture-prod@nourriture.dennajort.fr <<EOF
-  docker-compose -p "nourriturebjtu" pull && \
+  docker-compose -p "nourriturebjtu" pull --allow-insecure-ssl && \
   docker-compose -p "nourriturebjtu" up -d
 EOF
