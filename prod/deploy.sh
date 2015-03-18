cf91a243674ee2af7e7fe465c2c8ed84333d5763
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-03-18 18:42:15 +0800

    use env var

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp ./prod/docker-compose.yml ./prod/remove_dangling.sh nourriture-prod@nourriture.dennajort.fr && \
+scp ./prod/docker-compose.yml ./prod/remove_dangling.sh nourriture-prod@nourriture.dennajort.fr: && \
 ssh nourriture-prod@nourriture.dennajort.fr < ./prod/server.sh
