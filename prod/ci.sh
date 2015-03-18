f522fd2fa8050f5f2632b98f41273ef47e54c510
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-03-18 18:41:38 +0800

    use env var

--- prod/ci.sh
+++ prod/ci.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 FIG_PROJECT_NAME=nourriturebjtu
 echo "Build images" && \
-docker build --force-rm=true --pull=true -t docker.dennajort.fr/$FIG_PROJECT_NAME_api api/ && \
-docker build --force-rm=true --pull=true -t docker.dennajort.fr/$FIG_PROJECT_NAME_nginx www/ && \
+docker build --force-rm=true --pull=true -t docker.dennajort.fr/"$FIG_PROJECT_NAME"_api api/ && \
+docker build --force-rm=true --pull=true -t docker.dennajort.fr/"$FIG_PROJECT_NAME"_nginx www/ && \
 echo "Push images" && \
-docker push docker.dennajort.fr/$FIG_PROJECT_NAME_api && \
-docker push docker.dennajort.fr/$FIG_PROJECT_NAME_nginx && \
+docker push docker.dennajort.fr/"$FIG_PROJECT_NAME"_api && \
+docker push docker.dennajort.fr/"$FIG_PROJECT_NAME"_nginx && \
 echo "Run deploy script" && \
 sh ./prod/deploy.sh && \
 echo "Remove dangling" && \
